955849978320e0f6dfb82261e5aaec210c467be1
kauamiguel <kauamcm@gmail.com>
2026-05-20 05:57:22 -0300

    feat: add vpc, subnet and IGw

--- main.tf
+++ main.tf
@@ -1,9 +1,39 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  } 
+provider "aws"{
+  region = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+ 
+resource "aws_vpc" "prod-vpc"{
+  cidr_block = "10.0.0.0/16"
+}
 
-  required_version = ">= 1.2"
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+} 
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route = {
+    cid_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.prod-vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_route_table_association" "rt-subnet-1" {
+  subnet_id = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id = aws_vpc.prod-vpc.id
 }
